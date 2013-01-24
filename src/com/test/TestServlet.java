5e62432066c22ba03f5d3d100e02d3a7b850d3b5
Yaroslaw <dzharvis@gmail.com>
2013-01-24 19:42:54 +0200

    added jUnit

---
+++ src/com/test/TestServlet.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestServlet {
+	
+	@Test
+	public void testSomething(){
+		assertTrue(true);
+	}
+
+}
